82dc57d5a56b52ae814659d73842627e87904d72
Thiago Yukio Itagaki <thiago.itagaki@gmail.com>
2012-09-15 10:36:00 -0300

    JS Major File
    Upload contains some minor jquery mobile css and images files.

---
+++ mobile_app/js/js.js
@@ -0,0 +1,9 @@
+/*
+ *	BlackBerry 10 OS Tablet App - Hackathon Campinas / Brazil
+ *  App: 		"App Name"
+ *	Authors:	Andre Vitor Terron
+ *				Andre Seiji Tamanaha
+ *				Thiago Yukio Itagaki
+ *	Version: 	1.0.0.0
+ *	Data: 		15/09/2012
+ */
